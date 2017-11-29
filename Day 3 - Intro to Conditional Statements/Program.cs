87c81ada592bdf572592b4a1bdabd98156842d9d
Anthony H. Zhang <anthony.huazhang@gmail.com>
2017-11-29 12:21:44 -0500

    Day 3: Intro to Conditional Statements

---
+++ Day 3 - Intro to Conditional Statements/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+class Solution
+{
+    static bool checkIfOdd(int numberToCheck)
+    {
+        bool isOdd;
+        if (numberToCheck % 2 != 0)
+        {
+            isOdd = true;
+        }
+        else
+        {
+            isOdd = false;
+        }
+        return isOdd;
+    }
+    static void Main(String[] args)
+    {
+        int n = Convert.ToInt32(Console.ReadLine());
+        if (checkIfOdd(n) == true)
+        {
+            Console.WriteLine("Weird");
+        }
+        else
+        {
+            if (n >= 2 && n <= 5)
+            {
+                Console.WriteLine("Not Weird");
+            }
+            else if (n >= 6 && n <= 20)
+            {
+                Console.WriteLine("Weird");
+            }
+            else
+            {
+                Console.WriteLine("Not Weird");
+            }
+        }
+    }
+}
