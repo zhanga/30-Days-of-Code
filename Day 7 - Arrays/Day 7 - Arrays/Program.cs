6d83b23b65358546351123ceecba3ce101308ac5
Anthony H. Zhang <anthony.huazhang@gmail.com>
2017-12-04 18:32:26 -0500

    Updated Day 7 to use Array.Reverse() method.

--- Day 7 - Arrays/Day 7 - Arrays/Program.cs
+++ Day 7 - Arrays/Day 7 - Arrays/Program.cs
@@ -1,24 +1,11 @@
 ﻿using System;
 class Solution
 {
-    static int[] reverseArray(int[] arr, int[] tempArr, int n)
-    {
-        int reverseIndex = n - 1;
-        for (int i = 0; i < n; i++, reverseIndex--)
-            arr[i] = tempArr[reverseIndex];
-        return arr;
-    }
-
     static void Main(String[] args)
     {
         int n = Convert.ToInt32(Console.ReadLine());
         int[] arr = Array.ConvertAll(Console.ReadLine().Split(' '), Int32.Parse);
-        int[] tempArr = new int[n];
-
-        for (int i = 0; i < n; i++)
-            tempArr[i] = arr[i];
-
-        arr = reverseArray(arr, tempArr, n);
+        Array.Reverse(arr);
 
         foreach (var item in arr)
             Console.Write(item.ToString() + " ");
