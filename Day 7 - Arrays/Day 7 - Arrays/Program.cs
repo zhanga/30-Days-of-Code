112902c41c3470d472c2b0f6e96917781b44dfee
Anthony H. Zhang <anthony.huazhang@gmail.com>
2017-12-04 18:29:00 -0500

    Added Day 7 - Arrays.

---
+++ Day 7 - Arrays/Day 7 - Arrays/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+class Solution
+{
+    static int[] reverseArray(int[] arr, int[] tempArr, int n)
+    {
+        int reverseIndex = n - 1;
+        for (int i = 0; i < n; i++, reverseIndex--)
+            arr[i] = tempArr[reverseIndex];
+        return arr;
+    }
+
+    static void Main(String[] args)
+    {
+        int n = Convert.ToInt32(Console.ReadLine());
+        int[] arr = Array.ConvertAll(Console.ReadLine().Split(' '), Int32.Parse);
+        int[] tempArr = new int[n];
+
+        for (int i = 0; i < n; i++)
+            tempArr[i] = arr[i];
+
+        arr = reverseArray(arr, tempArr, n);
+
+        foreach (var item in arr)
+            Console.Write(item.ToString() + " ");
+    }
+}
