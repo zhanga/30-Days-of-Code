1588f22a20e24eb5dd37ef86b2f6ea686ff986aa
zhanga <anthony.huazhang@gmail.com>
2017-12-05 16:43:08 -0500

    Added Day 9 - Factorial

---
+++ Day 9 - Recursion/Day 9 - Recursion/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+class Solution
+{
+    static int factorial(int n)
+    {
+        if (n > 0)
+            return n * factorial(n - 1);
+        else
+            return 1;
+    }
+
+    static void Main(String[] args)
+    {
+        int n = Convert.ToInt32(Console.ReadLine());
+        int result = factorial(n);
+        Console.WriteLine(result);
+    }
+}
