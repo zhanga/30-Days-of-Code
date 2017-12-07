b4429e1e9c8995a3eaf906c5bfde2c2d740373be
zhanga <anthony.huazhang@gmail.com>
2017-12-06 19:33:44 -0500

    Added Day 10 - Binary Numbers.

---
+++ Day 10 - Binary Numbers/Day 10 - Binary Numbers/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+class Solution
+{
+    static int checkForConsecutiveOnes(string binaryValue)
+    {
+        int consecutiveOnes = 0;
+        int highestConsecutiveOnes = 0;
+        for (int i = 0; i < binaryValue.Length; i++)
+        {
+            if (binaryValue[i] == '1')
+            {
+                consecutiveOnes++;
+            }
+            else
+            {
+                if (highestConsecutiveOnes < consecutiveOnes)
+                    highestConsecutiveOnes = consecutiveOnes;
+                consecutiveOnes = 0;
+            }
+        }
+
+        if (highestConsecutiveOnes < consecutiveOnes)
+            highestConsecutiveOnes = consecutiveOnes;
+
+        return highestConsecutiveOnes;
+    }
+
+    static void Main(String[] args)
+    {
+        int n = Convert.ToInt32(Console.ReadLine());
+        string binaryValue = Convert.ToString(n, 2);
+        int consecutiveOnes = checkForConsecutiveOnes(binaryValue);
+        Console.WriteLine(consecutiveOnes);
+    }
+}
