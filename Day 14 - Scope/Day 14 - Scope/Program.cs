36cf9699e9f8f960b51341eef67733993b89d918
zhanga <anthony.huazhang@gmail.com>
2017-12-12 00:27:15 -0500

    Added Day 14 - Scope.

---
+++ Day 14 - Scope/Day 14 - Scope/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Linq;
+
+class Difference
+{
+    private int[] elements;
+    public int maximumDifference;
+
+    public Difference(int[] e)
+    {
+        elements = e;
+    }
+
+    public int computeDifference()
+    {
+        int minValue = 100;
+        int maxValue = 1;
+        for (int i = 0; i < elements.Length; i++)
+        {
+            if (elements[i] > maxValue)
+            {
+                maxValue = elements[i];
+            }
+            else if (elements[i] < minValue)
+            {
+                minValue = elements[i];
+            }
+        }
+        maximumDifference = Math.Abs(minValue - maxValue);
+        return maximumDifference;
+    }
+}
+    class Solution
+    {
+        static void Main(string[] args)
+        {
+            Convert.ToInt32(Console.ReadLine());
+
+            int[] a = Console.ReadLine().Split(' ').Select(x => Convert.ToInt32(x)).ToArray();
+
+            Difference d = new Difference(a);
+
+            d.computeDifference();
+
+            Console.Write(d.maximumDifference);
+        }
+    }
